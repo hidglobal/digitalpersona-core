152fd6f2345ebdc45a1904d93214e630fb231733
Andrey Bronnikov <andreyb@digitalpersona.com>
2019-05-29 18:56:09 -0700

    * fixed an issue with a user name type

--- src/users/user.ts
+++ src/users/user.ts
@@ -8,11 +8,11 @@ export class User
     public constructor(name: string, type?: UserNameType) {
         const reGUID = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/;
         this.name = name;
-        this.type = type ||
+        this.type = type || (
             (name.indexOf('@') !== -1)  ? UserNameType.UPN :
             (name.indexOf('\\') !== -1) ? UserNameType.SAM :
             reGUID.test(name)           ? UserNameType.UID
-                                        : UserNameType.DP;
+                                        : UserNameType.DP);
     }
 
     public static Anonymous(): User
